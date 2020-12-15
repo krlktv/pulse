d566b730717272745b0bcc54ff390159e1cdb43a
Kirill <krlktv@mail.ru>
2020-12-15 17:31:06 +0300

    update modal

--- src/js/script.js
+++ src/js/script.js
@@ -75,4 +75,11 @@ $(document).ready(function () {
 	$('.modal__close').on('click', function () {
 		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
 	});
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
 });
