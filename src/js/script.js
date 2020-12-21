245711fcef1d96bc4ce932ed7eee940cb325dee0
Kirill Kotov <krlktv@mail.ru>
2020-12-21 17:42:15 +0300

    update mailer

--- src/js/script.js
+++ src/js/script.js
@@ -114,6 +114,20 @@ $(document).ready(function () {
 	valideForms('#consultation form');
 	valideForms('#order form');
 
-
 	$('input[name=phone]').mask("+375-99-999-99-99");
+
+	$('form').submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });
